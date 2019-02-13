daaffeda83f669019ae31b18dc98e700e9fb9d37
aporquet <antoine.porquet34070@gmail.com>
2019-02-13 14:53:49 +0100

    add properties

--- PlanificationEntretien/src/main/java/fr/esgi/Candidat.java
+++ PlanificationEntretien/src/main/java/fr/esgi/Candidat.java
@@ -1,4 +1,11 @@
 package fr.esgi;
 
+import java.util.List;
+
+@Data
+
 public class Candidat {
+    private int Id;
+    private String Nom;
+    private List<Compétence> Competences;
 }
--- PlanificationEntretien/src/main/java/fr/esgi/ConsultantRecruteur.java
+++ PlanificationEntretien/src/main/java/fr/esgi/ConsultantRecruteur.java
@@ -1,4 +1,12 @@
 package fr.esgi;
 
+import java.util.List;
+
+@Data
+
 public class ConsultantRecruteur {
+
+    private int Id;
+    private String Nom;
+    private List<Compétence> Compétences;
 }
