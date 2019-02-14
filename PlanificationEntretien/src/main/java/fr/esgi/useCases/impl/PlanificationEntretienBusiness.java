625489145cb2679f3773b51c5c7e28d825b40286
JuanWick <jm.anghileri@wanadoo.fr>
2019-02-14 17:14:12 +0100

    ADD TODO planifier entretien

--- PlanificationEntretien/src/main/java/fr/esgi/useCases/impl/PlanificationEntretienBusiness.java
+++ PlanificationEntretien/src/main/java/fr/esgi/useCases/impl/PlanificationEntretienBusiness.java
@@ -7,7 +7,18 @@ import fr.esgi.useCases.PlanificationEntretiens;
 public class PlanificationEntretienBusiness implements PlanificationEntretiens {
     @Override
     public boolean planifierEntretien(Candidat candidat, CreneauHoraire creneauSouhaite) {
-        //TODO métier
+        //TODO vérifier candidat, creneauSouhaite
+
+        //TODO appel api
+
+        //TODO filter compétence consultant testeur
+
+        //TODO find first
+
+        //TODO creation entretien
+
+        //TODO percistance
+
         return false;
     }
 }
